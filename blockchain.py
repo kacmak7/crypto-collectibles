8f4044dc904a5e89dd9ff208ea04a5a2ae4793b7
Kacper Makuch <kacbull007@gmail.com>
2019-12-30 01:17:07 +0100

    format

--- blockchain.py
+++ blockchain.py
@@ -15,7 +15,7 @@ class Blockchain:
         self.nodes = set()
 
         # Create the genesis block
-        self.new_block(previous_hash='1', proof=100)
+        self.new_block(previous_hash="1", proof=100)
 
     def register_node(self, address):
         """
@@ -31,8 +31,7 @@ class Blockchain:
             # Accepts an URL without scheme like '192.168.0.5:5000'.
             self.nodes.add(parsed_url.path)
         else:
-            raise ValueError('Invalid URL')
-
+            raise ValueError("Invalid URL")
 
     def valid_chain(self, chain):
         """
@@ -47,16 +46,18 @@ class Blockchain:
 
         while current_index < len(chain):
             block = chain[current_index]
-            print(f'{last_block}')
-            print(f'{block}')
+            print(f"{last_block}")
+            print(f"{block}")
             print("\n-----------\n")
             # Check that the hash of the block is correct
             last_block_hash = self.hash(last_block)
-            if block['previous_hash'] != last_block_hash:
+            if block["previous_hash"] != last_block_hash:
                 return False
 
             # Check that the Proof of Work is correct
-            if not self.valid_proof(last_block['proof'], block['proof'], last_block_hash):
+            if not self.valid_proof(
+                last_block["proof"], block["proof"], last_block_hash
+            ):
                 return False
 
             last_block = block
@@ -80,11 +81,11 @@ class Blockchain:
 
         # Grab and verify the chains from all the nodes in our network
         for node in neighbours:
-            response = requests.get(f'http://{node}/chain')
+            response = requests.get(f"http://{node}/chain")
 
             if response.status_code == 200:
-                length = response.json()['length']
-                chain = response.json()['chain']
+                length = response.json()["length"]
+                chain = response.json()["chain"]
 
                 # Check if the length is longer and the chain is valid
                 if length > max_length and self.valid_chain(chain):
@@ -108,11 +109,11 @@ class Blockchain:
         """
 
         block = {
-            'index': len(self.chain) + 1,
-            'timestamp': time(),
-            'transactions': self.current_transactions,
-            'proof': proof,
-            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+            "index": len(self.chain) + 1,
+            "timestamp": time(),
+            "transactions": self.current_transactions,
+            "proof": proof,
+            "previous_hash": previous_hash or self.hash(self.chain[-1]),
         }
 
         # Reset the current list of transactions
@@ -130,13 +131,11 @@ class Blockchain:
         :param amount: Amount
         :return: The index of the Block that will hold this transaction
         """
-        self.current_transactions.append({
-            'sender': sender,
-            'recipient': recipient,
-            'amount': amount,
-        })
+        self.current_transactions.append(
+            {"sender": sender, "recipient": recipient, "amount": amount}
+        )
 
-        return self.last_block['index'] + 1
+        return self.last_block["index"] + 1
 
     @property
     def last_block(self):
@@ -165,7 +164,7 @@ class Blockchain:
         :return: <int>
         """
 
-        last_proof = last_block['proof']
+        last_proof = last_block["proof"]
         last_hash = self.hash(last_block)
 
         proof = 0
@@ -186,7 +185,7 @@ class Blockchain:
 
         """
 
-        guess = f'{last_proof}{proof}{last_hash}'.encode()
+        guess = f"{last_proof}{proof}{last_hash}".encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
 
@@ -195,13 +194,13 @@ class Blockchain:
 app = Flask(__name__)
 
 # Generate a globally unique address for this node
-node_identifier = str(uuid4()).replace('-', '')
+node_identifier = str(uuid4()).replace("-", "")
 
 # Instantiate the Blockchain
 blockchain = Blockchain()
 
 
-@app.route('/mine', methods=['GET'])
+@app.route("/mine", methods=["GET"])
 def mine():
     # We run the proof of work algorithm to get the next proof...
     last_block = blockchain.last_block
@@ -209,56 +208,51 @@ def mine():
 
     # We must receive a reward for finding the proof.
     # The sender is "0" to signify that this node has mined a new coin.
-    blockchain.new_transaction(
-        sender="0",
-        recipient=node_identifier,
-        amount=1,
-    )
+    blockchain.new_transaction(sender="0", recipient=node_identifier, amount=1)
 
     # Forge the new Block by adding it to the chain
     previous_hash = blockchain.hash(last_block)
     block = blockchain.new_block(proof, previous_hash)
 
     response = {
-        'message': "New Block Forged",
-        'index': block['index'],
-        'transactions': block['transactions'],
-        'proof': block['proof'],
-        'previous_hash': block['previous_hash'],
+        "message": "New Block Forged",
+        "index": block["index"],
+        "transactions": block["transactions"],
+        "proof": block["proof"],
+        "previous_hash": block["previous_hash"],
     }
     return jsonify(response), 200
 
 
-@app.route('/transactions/new', methods=['POST'])
+@app.route("/transactions/new", methods=["POST"])
 def new_transaction():
     values = request.get_json()
 
     # Check that the required fields are in the POST'ed data
-    required = ['sender', 'recipient', 'amount']
+    required = ["sender", "recipient", "amount"]
     if not all(k in values for k in required):
-        return 'Missing values', 400
+        return "Missing values", 400
 
     # Create a new Transaction
-    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+    index = blockchain.new_transaction(
+        values["sender"], values["recipient"], values["amount"]
+    )
 
-    response = {'message': f'Transaction will be added to Block {index}'}
+    response = {"message": f"Transaction will be added to Block {index}"}
     return jsonify(response), 201
 
 
-@app.route('/chain', methods=['GET'])
+@app.route("/chain", methods=["GET"])
 def full_chain():
-    response = {
-        'chain': blockchain.chain,
-        'length': len(blockchain.chain),
-    }
+    response = {"chain": blockchain.chain, "length": len(blockchain.chain)}
     return jsonify(response), 200
 
 
-@app.route('/nodes/register', methods=['POST'])
+@app.route("/nodes/register", methods=["POST"])
 def register_nodes():
     values = request.get_json()
 
-    nodes = values.get('nodes')
+    nodes = values.get("nodes")
     if nodes is None:
         return "Error: Please supply a valid list of nodes", 400
 
@@ -266,36 +260,32 @@ def register_nodes():
         blockchain.register_node(node)
 
     response = {
-        'message': 'New nodes have been added',
-        'total_nodes': list(blockchain.nodes),
+        "message": "New nodes have been added",
+        "total_nodes": list(blockchain.nodes),
     }
     return jsonify(response), 201
 
 
-@app.route('/nodes/resolve', methods=['GET'])
+@app.route("/nodes/resolve", methods=["GET"])
 def consensus():
     replaced = blockchain.resolve_conflicts()
 
     if replaced:
-        response = {
-            'message': 'Our chain was replaced',
-            'new_chain': blockchain.chain
-        }
+        response = {"message": "Our chain was replaced", "new_chain": blockchain.chain}
     else:
-        response = {
-            'message': 'Our chain is authoritative',
-            'chain': blockchain.chain
-        }
+        response = {"message": "Our chain is authoritative", "chain": blockchain.chain}
 
     return jsonify(response), 200
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
-    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    parser.add_argument(
+        "-p", "--port", default=5000, type=int, help="port to listen on"
+    )
     args = parser.parse_args()
     port = args.port
 
-    app.run(host='0.0.0.0', port=port)
+    app.run(host="0.0.0.0", port=port)
